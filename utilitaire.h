0694e51f4579560b91dfcf19a0444867ab2bafed
FeyereisenThomas2TL2 <32071512+FeyereisenThomas2TL2@users.noreply.github.com>
2018-08-29 12:47:24 +0200

    Update utilitaire.h

--- utilitaire.h
+++ utilitaire.h
@@ -1,21 +1,58 @@
-/*
- * Commencer les essais
- */
+#ifndef utilitaire_h
+#define utilitaire_h
+#include "voiture.h"
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//déclaration des variables utiles
+int nbrLapMax;
+int lengthCircuit;
+structCar *cars;
+int *smv;//Tableau des variables globales partagées par tous les processus
+int *pidList;
+int id_sem;
+structCar *carsQualif2;
+structCar *carsQualif3;
+int startPosition[20];//Ajouter le numéro des voitures
+int isRace;//Booleen dans le but de savoir si on est en course ou pas
+
+//Structures des sémaphores utilisées
+extern struct sembuf semWait;
+extern struct sembuf semDo;
+extern struct sembuf semPost;
+extern struct sembuf semWait1;
+extern struct sembuf semDo1;
+extern struct sembuf semPost1;
+extern struct sembuf semWait2;
+extern struct sembuf semDo2;
+extern struct sembuf semPost2;
+
+//déclaration des fonctions utiles
+int generateRandom(int min_number, int max_number);
+int pitStop(int i);
+void crash(int index);
+double getCurrTime();//temps maximum des voitures
+int nbrLaps(int km);
+int indexOf(int i, int length, int t[]);//faire correspondre pId et numVoiture
+int isIn(int name, int length, structCar t[]);//vérification si la voiture est dans le tableau
+void generateTimeS1(int i);
+void generateTimeS2(int i);
+void generateTimeS3(int i);
+char askAction();//demande si continue ou stop
+void interaction(int i);//demande à l'utilisateur s'il faut arreter le processus
+void killChildren();
+void wakeChildren();
+void sortRace(structCar carsQualif[], int sizeArrayCars);//trier les voitures sur base de leur temps actuel
+void addTimeByPosition();//ajout de temps en fonction de la position
+void setOut(int q);//modifier les variables des voitures à 1
+
 void practice(int index, int p);
-/*
- * Créer une feuille de résultats
- */
 void generateRecapFilePractice();
-/*
- * Trie les voitures en fonction de leur temps et du nombre de tour
- */
 void sortCarsByCurrTime(structCar carsRace[]);
-/*
- * Lance la course sur carsRace
- */
 void race(int index);
-/*
- * Créé un fichier avec le récap de la course
- */
 void generateRecapFileRace();
 
+#endif // utilitaire_h 
