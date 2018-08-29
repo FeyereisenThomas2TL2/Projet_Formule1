15b6f99c8fe726e9c7d41008273fce18b9e4a190
FeyereisenThomas2TL2 <32071512+FeyereisenThomas2TL2@users.noreply.github.com>
2018-08-29 11:51:16 +0200

    Update utilitaire.h

--- utilitaire.h
+++ utilitaire.h
@@ -10,12 +10,10 @@ void generateRecapFilePractice();
  * Trie les voitures en fonction de leur temps et du nombre de tour
  */
 void sortCarsByCurrTime(structCar carsRace[]);
-
 /*
  * Lance la course sur carsRace
  */
 void race(int index);
-
 /*
  * Créé un fichier avec le récap de la course
  */
